ef569d64ed155874c4eef04249f8c8aa720cedb1
Quoc Le <quocle@chotot.vn>
2022-04-11 12:19:46 +0700

    init project

---
+++ weather-forecast/weather_forecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_forecastApp.swift
+//  weather-forecast
+//
+//  Created by Kenz on 11/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct weather_forecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
